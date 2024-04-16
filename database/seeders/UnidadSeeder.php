62097ccb8c909bf342d335f6a7c7156af891604f
Alvaro Aruquipa <alvarolp1@gmail.com>
2024-04-16 15:31:58 -0400

    Update UnidadSeeder.php

--- database/seeders/UnidadSeeder.php
+++ database/seeders/UnidadSeeder.php
@@ -16,7 +16,7 @@ class UnidadSeeder extends Seeder
      */
     public function run()
     {
-        $table = new TableReader(public_path('dbfs/unidadadmin.dbf'),['encoding' => 'cp1251']);
+        $table = new TableReader(public_path('dbfs/unidadadmin.DBF'),['encoding' => 'cp1251']);
         while ($record = $table->nextRecord()) {
             DB::table('unidadadmin')->insert([
             'entidad' =>$record->get('entidad'),
